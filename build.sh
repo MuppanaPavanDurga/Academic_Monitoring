1989dfc7c0031acaf2849f9b1f5f1a7381545b30
MuppanaPavanDurga <muppana.pavandurga@gmail.com>
2026-02-14 09:46:48 +0530

    Auto create admin user during deploy

--- build.sh
+++ build.sh
@@ -5,5 +5,16 @@ set -o errexit
 pip install -r requirements.txt
 
 cd academic_monitoring
-python manage.py collectstatic --no-input
+
+# apply database migrations
 python manage.py migrate
+
+# create superuser automatically (only if not exists)
+echo "from django.contrib.auth import get_user_model; \
+User=get_user_model(); \
+User.objects.filter(username='admin').exists() or \
+User.objects.create_superuser('admin','admin@gmail.com','admin12345')" \
+| python manage.py shell
+
+# collect static files
+python manage.py collectstatic --no-input
